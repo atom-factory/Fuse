4168b1a3cde73e5a09a18375443977df63a39ae7
Jake Rieger <contact.jakerieger@gmail.com>
2024-07-16 08:45:04 -0400

    cmake config changes

--- Docs/html/dir_d4b5106c91502fbd840a5357c642b335.js
+++ Docs/html/dir_d4b5106c91502fbd840a5357c642b335.js
@@ -2,6 +2,6 @@ var dir_d4b5106c91502fbd840a5357c642b335 =
 [
     [ "Direct2DBackend.cpp", "_direct2_d_backend_8cpp.html", null ],
     [ "Direct2DBackend.h", "_direct2_d_backend_8h.html", "_direct2_d_backend_8h" ],
-    [ "X11Backend.cpp", "_x11_backend_8cpp.html", null ],
-    [ "X11Backend.h", "_x11_backend_8h.html", "_x11_backend_8h" ]
+    [ "CairoBackend.cpp", "_x11_backend_8cpp.html", null ],
+    [ "CairoBackend.h", "_x11_backend_8h.html", "_x11_backend_8h" ]
 ];
