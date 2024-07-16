20796850482c4ca374920ee0daa1661345eb75a7
Jake Rieger <contact.jakerieger@gmail.com>
2024-07-16 15:07:17 -0400

    wayland integration wip

--- Examples/WaylandParent.h
+++ Examples/WaylandParent.h
@@ -4,14 +4,19 @@
 
 #pragma once
 
-#include <iostream>
-
 #include "Types.h"
 #include "Platform/Platform.h"
 #include "Platform/WaylandPluginView.h"
 
 namespace WaylandParent {
     static inline ArkVector::WaylandPluginView* g_View;
+
+    static wl_display* g_Display       = nullptr;
+    static wl_compositor* g_Compositor = nullptr;
+    static wl_surface* g_Surface       = nullptr;
+    static wl_subsurface* g_Subsurface = nullptr;
+    static wl_egl_window* g_EglWindow  = nullptr;
+    static xdg_wm_base* g_XdgWmBase    = nullptr;
 
     static int CreateParentWindow() {
         return 0;
